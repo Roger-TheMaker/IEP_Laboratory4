54239cc502f7d0d9737e97c7638743d450624bd8
Roger <rogernuica3@gmail.com>
2021-10-31 16:09:54 +0200

    Singleton like Class

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ class Avion
   {
 	  planeId = id;
 	  totalEnginePower = eng;
+	  std::cout << "calling the class Avion constructor" << std::endl;
   }
 
   virtual std::string getPlaneID()
@@ -46,6 +47,7 @@ class Calatori : public Avion
 	  Calatori(const std::string &id, int eng, int maxPassengers) : Avion(id,eng)
 	  {
 		  this->maxPassengers = maxPassengers;
+		  std::cout << "calling the class Calatori constructor" << std::endl;
 	  }
 
 	  virtual int getMaxPassengers()
@@ -67,19 +69,61 @@ class Calatori : public Avion
   public:
 	  Lupta(const std::string &id, int eng) : Avion(id, eng)
 	  {
+	 	 std::cout << "calling the class Lupta constructor" << std::endl;
 	  }
 	  virtual void launchMissile()
 	  {
 
-		  std::cout << getPlaneID() << getTotalEnginePower() << " --- Fire! " << std::endl;
+		  std::cout << getPlaneID() << getTotalEnginePower() << " Prepare missile --- Fire! " << std::endl;
 	  }
 
   };
   
   
+ 
+class AvionUnic
+{
+    private:
+        /* Here will be the instance stored. */
+        static AvionUnic* instance;
+
+        /* Private constructor to prevent instancing. */
+        AvionUnic();
+
+    public:
+        /* Static access method. */
+        static AvionUnic* getInstance();
+};
+
+/* Null instance, because it will be initialized on demand. */
+
+AvionUnic* AvionUnic::instance = 0; //declararea unui atribut
+
+AvionUnic* AvionUnic::getInstance() //declararea unei metode
+{
+    if (instance == 0)
+    {
+        instance = new AvionUnic();
+        std::cout << "first and only instantion of the class AvionUnic"  << std::endl;
+        
+    }
+
+    return instance;
+}
+
+AvionUnic::AvionUnic() // definirea constructorului
+{
+	std::cout << "calling the constructor of AvionUnic"  << std::endl;
+}
+
+
+  
 
 int main(int argc, char** argv){
 	std::cout << "Hello Roger" << std::endl;
+	
+	Avion avion7("Roger's Plane ", 2);
+	avion7.printDetails();
 	
 	Avion avion1 = Avion("Alcatraz", 2);
 	avion1.printDetails();
@@ -87,9 +131,21 @@ int main(int argc, char** argv){
 	Calatori avion2 = Calatori("Air700Ro", 7 , 30);
 	avion2.printDetails();
 	
-	
 	Avion avion3 = Calatori("Air700Ro2", 7 , 30);
 	avion3.printDetails();
+	
+	Lupta avion4 = Lupta("Air700Ro2", 7);
+	avion4.launchMissile();
+	
+	Avion avion5 = Lupta("Air700Ro2", 7);
+	//avion5.launchMissile();
+	
+	AvionUnic* z = AvionUnic::getInstance();
+	std::cout << "Hascode: "   <<  z  << std::endl;
+	
+	AvionUnic* w = AvionUnic::getInstance();
+	std::cout << "Hascode: "   <<  w  << std::endl;
+	
 	return 0;
 }
 
