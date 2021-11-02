b95d15ef9d16bff050da1c153f4d1d608d74490a
Roger <rogernuica3@gmail.com>
2021-11-02 19:27:45 +0200

    Commentaries and modifications

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
-#include <string>
+#include <string> //necesar pentru citire sau scriere la iesirea/intrarea standard
 #include <iostream>
 
 class Avion
 {
-  private:
-  std::string planeId;
-  int totalEnginePower;
+  protected:
+  std::string planeId = "";
+//std este un namespace care inseamna o regiune declararativa pentru nume de topuri, functii si variabile
+// :: este un operator numit "scope" care precizeaza compilatorului dupa ce nume sa se uite in lista de identificatori
+  int totalEnginePower = 0;
 
   public:
-  Avion(const std::string &id, int eng)
-  {
-	  planeId = id;
-	  totalEnginePower = eng;
-	  std::cout << "calling the class Avion constructor" << std::endl;
+  Avion(const std::string &id, int eng): planeId(id), totalEnginePower(eng)
+  {  	  //Atribuirea proprietatilor constructorului folosind Lista de Initializare
+	  
+	  std::cout << "calling the constructor of class Avion" << std::endl;
   }
+  
+  
+  /*
+  or
+  
+  Avion(const std::string &id, int eng)
+  {  	  
+	  this->planeId = id;
+	  this->totalEnginePower = eng;
+	  std::cout << "calling the constructor of class Avion" << std::endl;
+  }
+
+  */
 
   virtual std::string getPlaneID()
   {
@@ -30,7 +44,7 @@ class Avion
 
   virtual void printDetails()
   {
-	  std::cout << getPlaneID() << " " << getTotalEnginePower() << std::endl;
+	  std::cout << "Plane ID: " << getPlaneID() << " | TotalEnginePower: " << getTotalEnginePower() << std::endl;
   }
 };
 
@@ -40,14 +54,14 @@ class Avion
 class Calatori : public Avion
 {
 
-  private:
+  protected:
 	  int maxPassengers;
 
   public:
 	  Calatori(const std::string &id, int eng, int maxPassengers) : Avion(id,eng)
 	  {
 		  this->maxPassengers = maxPassengers;
-		  std::cout << "calling the class Calatori constructor" << std::endl;
+		  std::cout << "calling the constructor of class Calatori" << std::endl;
 	  }
 
 	  virtual int getMaxPassengers()
@@ -55,10 +69,10 @@ class Calatori : public Avion
 		  return this->maxPassengers;
 	  }
 
-	 virtual void printDetails()
-  {
-	  std::cout << getPlaneID() << " " << getTotalEnginePower() << " " << getMaxPassengers() << std::endl;
-  }
+	 virtual void printDetails() override
+  		{
+		   std::cout << "Plane ID: " << getPlaneID() << " | TotalEnginePower: " << getTotalEnginePower() << " | MaxNumberOfPassengers: " << getMaxPassengers() << std::endl;
+  		}
 };
 
 
@@ -69,62 +83,87 @@ class Calatori : public Avion
   public:
 	  Lupta(const std::string &id, int eng) : Avion(id, eng)
 	  {
-	 	 std::cout << "calling the class Lupta constructor" << std::endl;
+	 	 std::cout << "calling the constructor of class Lupta" << std::endl;
 	  }
 	  virtual void launchMissile()
 	  {
 
-		  std::cout << getPlaneID() << getTotalEnginePower() << " Prepare missile --- Fire! " << std::endl;
+		std::cout << "Plane ID: " << getPlaneID() << " | TotalEnginePower: " << getTotalEnginePower() <<  " | Prepare missile --- Fire! "  << std::endl;
 	  }
 
   };
   
   
+  
+  
+  class Avion_Testare : public Avion
+  {
+
+  public:
+	  Avion_Testare(const std::string &id, int eng) : Avion(id, eng)
+	  {
+	 	 std::cout << "calling the constructor of class Avion_Testare" << std::endl;
+	  }
+	  
+	virtual void printDetails() override
+  	  {
+	 	 std::cout << getPlaneID() << " " << getTotalEnginePower() << std::endl;
+          }
+
+  };
+  
+  
+  
+  
  
-class AvionUnic
+class AvionPrivat
 {
     private:
         /* Here will be the instance stored. */
-        static AvionUnic* instance;
+        static AvionPrivat* instance;
 
         /* Private constructor to prevent instancing. */
-        AvionUnic();
+        AvionPrivat();
 
     public:
         /* Static access method. */
-        static AvionUnic* getInstance();
+        static AvionPrivat* getInstance();
+        
+        virtual void print()  // simple method
+  	  {
+	 	 std::cout << "Just For VIPS" << std::endl;
+          }
 };
 
+
+
+AvionPrivat* AvionPrivat::instance = 0;
 /* Null instance, because it will be initialized on demand. */
 
-AvionUnic* AvionUnic::instance = 0; //declararea unui atribut
-
-AvionUnic* AvionUnic::getInstance() //declararea unei metode
+AvionPrivat* AvionPrivat::getInstance()
 {
     if (instance == 0)
     {
-        instance = new AvionUnic();
-        std::cout << "first and only instantion of the class AvionUnic"  << std::endl;
+        instance = new AvionPrivat();
+        std::cout << "first and only instantion of the class AvionPrivat"  << std::endl;
         
     }
 
     return instance;
 }
 
-AvionUnic::AvionUnic() // definirea constructorului
+AvionPrivat::AvionPrivat() // definirea constructorului
 {
-	std::cout << "calling the constructor of AvionUnic"  << std::endl;
+	std::cout << "calling the constructor of AvionPrivat"  << std::endl;
 }
 
 
   
 
 int main(int argc, char** argv){
-	std::cout << "Hello Roger" << std::endl;
+	std::cout << "Welcome to the Plane Management System" << std::endl;
 	
-	Avion avion7("Roger's Plane ", 2);
-	avion7.printDetails();
-	
+
 	Avion avion1 = Avion("Alcatraz", 2);
 	avion1.printDetails();
 	
@@ -134,17 +173,25 @@ int main(int argc, char** argv){
 	Avion avion3 = Calatori("Air700Ro2", 7 , 30);
 	avion3.printDetails();
 	
-	Lupta avion4 = Lupta("Air700Ro2", 7);
+	Lupta avion4 = Lupta("rUSSIA", 7);
 	avion4.launchMissile();
+	avion4.printDetails();
 	
-	Avion avion5 = Lupta("Air700Ro2", 7);
-	//avion5.launchMissile();
 	
-	AvionUnic* z = AvionUnic::getInstance();
+	
+	AvionPrivat* z = AvionPrivat::getInstance();
 	std::cout << "Hascode: "   <<  z  << std::endl;
 	
-	AvionUnic* w = AvionUnic::getInstance();
+	AvionPrivat* w = AvionPrivat::getInstance();
 	std::cout << "Hascode: "   <<  w  << std::endl;
+	z->print();
+	
+	
+	Avion_Testare * avion_testare = new Avion_Testare("Avion de Testare", 20);
+	avion_testare->printDetails(); // MERGE SI ASA
+	
+	Avion * avion10 = new Lupta("Americano", 2);
+	//avion10->launchMissile(); Este corect ca nu merge
 	
 	return 0;
 }
